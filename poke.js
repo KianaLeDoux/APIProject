1806f9000c17ab6765728625b7212935d1ef8280
Kiana LeDoux <kianaceleste@Kianas-MacBook-Air.local>
2019-08-20 14:35:23 -0400

    APIProject

---
+++ poke.js
@@ -0,0 +1,9 @@
+const baseURL = 'https://pokeapi.co/docs/v2.html#pokemon';
+let url = 'https://pokeapi.co/api/v2/pokemon/'; 
+
+
+const searchTerm = document.querySelector('.search type');
+const searchForm = document.querySelector('.form');
+const submitBtn = document.querySelector('.submit');
+const nextBtn = document.querySelector('.next');
+
